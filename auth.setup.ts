d5c803c82f860647f5d31f418616fbf9960b6b94
DmitriyKoshelev <koshelev.dmitryy@gmail.com>
2025-07-18 19:14:07 +0300

    fix setup file

--- auth.setup.ts
+++ auth.setup.ts
@@ -2,7 +2,7 @@
 import { chromium } from '@playwright/test';
 import fs from 'fs';
 
-(async () => {
+async function globalSetup () {
   const browser = await chromium.launch({ headless: true });
   const context = await browser.newContext();
   const page = await context.newPage();
@@ -10,17 +10,16 @@ import fs from 'fs';
   // Перейти на сайт
   await page.goto('https://www.zara.com/ua');
 
-  // ✅ Автоматично прийняти cookies, якщо банер з’явився
-  const acceptCookiesSelector = 'button:has-text("Усі прийняти")'; // або англ. "Accept all", якщо мова інша
+  //Автоматично прийняти cookies, якщо банер з’явився
+  const acceptCookiesSelector = 'button:has-text("Усі прийняти")';
 
   if (await page.locator(acceptCookiesSelector).isVisible({ timeout: 5000 }).catch(() => false)) {
     await page.click(acceptCookiesSelector);
     console.log('Cookies прийняті автоматично.');
-  } else {
-    console.log('Cookies банер не знайдено.');
   }
 
-  // ✅ Зберігаємо сесію
+  //Зберігаємо сесію
   await context.storageState({ path: './data/storageState.json' });
   await browser.close();
-})();
+};
+export default globalSetup;
